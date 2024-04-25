183698804247fb164aa3432ad5f37e3c3ad2d5e3
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-04-25 17:16:12 -0300

    feat: initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="bg-amber-600">
+            <h1>Header</h1>
+        </div>
+    );
+}
+
+export default Header;
