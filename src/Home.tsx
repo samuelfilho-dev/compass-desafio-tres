5927576f325428e097e81c67b034c791db2a621f
samuelfilho-dev <snamuelfilho2011@gmail.com>
2024-04-25 21:15:07 -0300

    feat: add Home Screen

---
+++ src/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Banner from "./components/banner/Banner";
+
+function Home() {
+    return (
+        <div>
+            <Header/>
+            <Banner/>
+        </div>
+    );
+}
+
+export default Home;
